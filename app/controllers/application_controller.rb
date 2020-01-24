35fa8cb19744cb13cd0df2e968a61704830e674b
Angelo Spampinato <spampinatodesign@gmail.com>
2020-01-23 23:22:01 -0500

    adding includes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+include ActionController::HttpAuthentication::Basic::ControllerMethods
+include ActionController::HttpAuthentication::Token::ControllerMethods
+
 class ApplicationController < ActionController::API
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
