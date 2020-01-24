05c6b79a225a301a924bbca6a7bc5c55022259fd
Angelo Spampinato <spampinatodesign@gmail.com>
2020-01-23 23:35:57 -0500

    trying 1 more time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
     include ActionController::HttpAuthentication::Token::ControllerMethods
 
     def authenticate
-      authenticate_or_request_with_http_basic do |username, password|
+      authenticate_or_request_with_http_token do |username, password|
       username == "admin" && password == "test"
     end
 end
