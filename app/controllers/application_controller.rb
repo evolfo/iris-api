2d55978df2c276fc7d83e70e10067c22a0d8d473
Angelo Spampinato <spampinatodesign@gmail.com>
2020-01-23 23:18:38 -0500

    trying to remove protected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::API
-  protected
     def authenticate
       authenticate_or_request_with_http_basic do |username, password|
       username == "admin" && password == "test"
     end
-  end
 end
